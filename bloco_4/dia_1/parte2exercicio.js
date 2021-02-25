6a9edd8e5266c558ba6947e3ed1e60ed71584c31
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-02-25 16:43:12 -0300

    Parte 2

---
+++ bloco_4/dia_1/parte2exercicio.js
@@ -0,0 +1,10 @@
+const nome = "Jefferson Andrade";
+const birthCity = "São Paulo";
+let birthYear = 1991;
+
+birthYear = 2020
+birthCity = "São Paulo"
+
+console.log(nome)
+console.log(birthYear)
+console.log(birthCity)
