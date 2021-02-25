ce69a838cb8fd06ffc4eaddecb11cc5e53897972
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-02-25 19:18:57 -0300

    Exercicio parte 3

---
+++ bloco_4/dia_1/parte3exercicio.js
@@ -0,0 +1,17 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+    firstName: 'Ana',
+    lastName: 'Santos',
+};
+
+const patientEmail = 'ana@email.com';
+console.log(typeof patientId)
+ // Área Triangulo
+ let base = 5;
+ let altura = 8;
+ let area = (base * altura) 
+ console.log(area)
+
+ let perimetro = (base + altura)*2
+ console.log(perimetro) 
