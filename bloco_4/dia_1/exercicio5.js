d2e7d5675c609dd163d2f0a78b9d0689dd5e1c83
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-02-26 07:34:18 -0300

    Exercicio 5

---
+++ bloco_4/dia_1/exercicio5.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = -2;
+let c = 150;
+
+if(a>0 && b>0 && c>0){
+  let soma = a + b + c
+  if(soma == 180){
+    console.log('True')
+  } else {
+    console.log('False')
+
+  }
+    
+}else{
+  console.log('Os ângulos precisam ser maiores que 0!')
+}
+
