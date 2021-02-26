a9d8af123aff9a6105b62bc401173b28a8336bf6
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-02-26 09:26:58 -0300

    Exercicio 9

--- bloco_4/dia_1/exercicio8.js
+++ bloco_4/dia_1/exercicio8.js
@@ -2,7 +2,7 @@ let num1=1;
 let num2=1;
 let num3=1;
 
-if((num1 % 2 ==0)|| (num2 % 2 == 0) || (num3 % 2 ==0)){
+if((num1 % 2 == 0)|| (num2 % 2 == 0) || (num3 % 2 ==0)){
     console.log('True')
 }else{
     console.log("False")
---
+++ bloco_4/dia_1/exercicio9.js
@@ -0,0 +1,9 @@
+let num1=2;
+let num2=2;
+let num3=2;
+
+if((num1 % 2 != 0)|| (num2 % 2 != 0) || (num3 % 2 !=0)){
+    console.log('True')
+}else{
+    console.log("False")
+}
