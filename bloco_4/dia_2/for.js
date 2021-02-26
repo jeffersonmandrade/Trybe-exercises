40de76ed8f440f0855b5f280fa34344bba2de022
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-02-26 13:41:21 -0300

    Conteudo For

---
+++ bloco_4/dia_2/for.js
@@ -0,0 +1,15 @@
+for(let index = 0; index<11;index++){
+    let multi = index *9
+    console.log(multi)
+}
+let a = "nome"
+console.log(a)
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+for(let i=0; i < cars.length;i++){
+    console.log(cars[i])
+}
+ let grocerylist = ['Arroz','Feijão', 'Alface', 'Melancia'];
+ for(let index =0; index < grocerylist.length; index++){
+     console.log(grocerylist[index])
+ }
