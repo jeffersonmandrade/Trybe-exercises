b6232ef4547a368f4ece7d421a9e84d3501734e2
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-02-28 16:23:00 -0300

    Exercicio 1

---
+++ bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,22 @@
+let n =5;
+let str = "*";
+let linha = "" ;
+
+if( n > 1){
+for(let index=0 ; index < n;index++){
+  linha += str
+}
+for(let index=0; index < n;index++){
+  console.log(linha)
+}}else{
+  console.log('ERRO, Valor de n Inválido.')
+}
+
+// Metodo usando .repeat
+// if(n > 1){
+// for(let index = 0; index < n;index++){
+//   console.log(str.repeat(n))
+// }
+// }else{
+//   console.log('ERRO, Valor de n Inválido.')
+// }
