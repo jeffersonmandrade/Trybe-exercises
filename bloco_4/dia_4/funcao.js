c64d09978466d4c6bc09c3990d9b67139229f539
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-01 15:57:12 -0300

    Exercicios bloco_4

---
+++ bloco_4/dia_4/funcao.js
@@ -0,0 +1,34 @@
+function sum(a,b){
+  let soma = a + b;
+  return soma;
+};
+
+console.log(sum(2,3))
+
+function sub(a,b){
+  let subtracao = a - b;
+  return subtracao;
+};
+
+console.log(sub(5,2))
+
+function mult(a,b){
+  let multiplicacao = a*b
+  return multiplicacao
+}
+
+console.log(mult(2,1))
+
+function div(a,b){
+  let divisao = a/b
+  return divisao
+}
+
+console.log(div(2,1))
+
+function mod(a,b){
+  let multiplicacao = a%b
+  return multiplicacao
+}
+
+console.log(mod(3,2))
