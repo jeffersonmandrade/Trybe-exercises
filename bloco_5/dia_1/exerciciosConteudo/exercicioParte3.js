45536a8314dc420bd24e6fc498ea56cb8d88c773
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-04 18:08:12 -0300

    Começando os exercicios

---
+++ bloco_5/dia_1/exerciciosConteudo/exercicioParte3.js
@@ -0,0 +1,15 @@
+document.getElementById('header-container').style.backgroundColor = 'yellow';//
+document.getElementById('header-container').style.color = 'black';
+document.getElementsByTagName('div')[0].style.background = 'orange'
+document.getElementsByTagName('div')[1].style.background = 'orange'
+document.getElementsByTagName('h3')[0].style.background = 'black'
+document.getElementsByTagName('h3')[1].style.background = 'black'
+
+document.getElementsByClassName('emergency-tasks')[0].style.background = 'orange'
+document.getElementsByClassName('no-emergency-tasks')[0].style.background = 'blue'
+
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black'
+
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black'
+document.querySelector('#footer-container').style.background = 'green'
+document.querySelector('#header-container').style.border = 'solid'
