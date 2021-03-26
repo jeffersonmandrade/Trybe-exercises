645915dd9659de2bf4b82e984e93b0b920f861bd
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-26 09:34:43 -0300

    exercicio 3 feito

---
+++ bloco_7/dia_3/exmplos.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+const soma = (a, b) => a + b;
+
+assert.strictEqual(soma(6,2), 8);
+
+const divisao = (c, d) => c /  d;
+
+
+assert.strictEqual (divisao(8,2), 4);
