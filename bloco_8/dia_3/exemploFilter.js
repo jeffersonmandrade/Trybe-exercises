05e7eef8218efdf424a2cff734272c2ec6410bf0
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-04-05 20:28:40 -0300

    Exercicios Terminados

---
+++ bloco_8/dia_3/exemploFilter.js
@@ -0,0 +1,25 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.filter(verifyEven);
+
+console.log(isEven); // [ 30, 22 ]
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.filter((number) => number % 2 === 0);
+
+console.log(isEven2); // [ 30, 22 ]
+
+const objPeople = [
+  { name: 'José', age: 21 },
+  { name: 'Lucas', age: 19 },
+  { name: 'Maria', age: 16 },
+  { name: 'Gilberto', age: 18 },
+  { name: 'Vitor', age: 15 },
+];
+console.log(objPeople.filter((element) => element.age < 18 ))
+
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+console.log(arrayMyStudents.filter((element)=> element != 'Ricardo'))
