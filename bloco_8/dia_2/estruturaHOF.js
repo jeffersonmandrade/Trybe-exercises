f1fd7e4602464568774939614b09ed5499323ed7
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-04-05 12:35:57 -0300

    Exercicio 3 feito

--- bloco_8/dia_2/estruturaHOF.js
+++ bloco_8/dia_2/estruturaHOF.js
@@ -1,9 +1,12 @@
 const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
-arrayOfValues.forEach((element) => {
+arrayOfValues.forEach((element,index,array) => {
+  console.log(index)
   console.log('Cada elemento do array:', element);
+  console.log(array)
 });
 
 // Cada elemento do array: josé
 // Cada elemento do array: 50
 // Cada elemento do array: 0.25
 // Cada elemento do array: { comida: 'Chocolate' }
+
